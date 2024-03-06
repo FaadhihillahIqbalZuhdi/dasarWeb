63c51519f920491bb137acd50b412174ab6b11ac
Faadhihillah Iqbal Zuhdi <iqbalzfaadhihillah@gmail.com>
2024-03-06 08:44:31 +0700

    Praktikum 4

--- Jobsheet4/struktur_kontrol.php
+++ Jobsheet4/struktur_kontrol.php
@@ -44,5 +44,54 @@ foreach ($skorUjian as $skor) {
     $totalSkor += $skor;
 }
 
-echo "Total skor ujian adalah: $totalSkor"
+echo "Total skor ujian adalah: $totalSkor";
+echo "<br><br>";
+
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai < 60) {
+        echo "Nilai: $nilai (Tidak lulus) <br>";
+        continue;
+    }
+}
+echo "Nilai $nilai (Lulus) <br><br>";
+
+// Tugas 4.6
+$nilaiSiswa1 = [85, 92, 78, 64, 90, 75, 88, 79, 70, 96];
+
+sort($nilaiSiswa1);
+
+$totalNilai = 0;
+$jumlahNilai = count($nilaiSiswa1);
+
+for ($i = 1; $i < $jumlahNilai - 2; $i++) {
+    $totalNilai += $nilaiSiswa1[$i];
+}
+
+echo "Total nilai setelah mengabaikan dua nilai tertinggi dan 
+dua nilai terendah adalah: $totalNilai <br><br>";
+
+// Tugas 4.7
+$hargaProduk = 120000;
+$diskon = 0.20; 
+
+if ($hargaProduk > 100000) {
+    $hargaSetelahDiskon = $hargaProduk - ($hargaProduk * $diskon);
+    echo "Harga Produk Setelah Diskon: $hargaSetelahDiskon";
+} else {
+    echo "Harga Produk: $hargaProduk";
+}
+echo "<br><br>";
+
+// Tugas 4.8
+$skorPemain = 750; 
+
+echo "Total skor pemain adalah: $skorPemain<br>";
+
+if ($skorPemain > 500) {
+    echo "Apakah pemain mendapatkan hadiah tambahan? YA";
+} else {
+    echo "Apakah pemain mendapatkan hadiah tambahan? TIDAK";
+}
 ?>
